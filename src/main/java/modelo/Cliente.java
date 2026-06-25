ec40ad632da00f4d647d800638acd1ac64a9c5cc
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-25 16:12:00 -0500

    commit - Cliente .

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -2,4 +2,52 @@ package modelo;
 
 public class Cliente
 {
-}
+    private Integer id;
+    private String nombre;
+    private String email;
+
+    // constructor vacio .
+    public Cliente() {
+
+    }
+
+    // constructor con parametros .
+    public Cliente(Integer id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}   
