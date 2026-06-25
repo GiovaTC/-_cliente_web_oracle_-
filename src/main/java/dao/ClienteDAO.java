5c1a5c3ad6fd91bfd33b0b9e88bc73b969c1a236
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-25 16:26:00 -0500

    commit - ClienteService .

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -72,5 +72,5 @@ public class ClienteDAO {
             e.printStackTrace();
         }
         return lista;
-    }   
+    }
 }
--- src/main/java/servicio/ClienteService.java
+++ src/main/java/servicio/ClienteService.java
@@ -1,4 +1,35 @@
 package servicio;
 
+import dao.ClienteDAO;
+import modelo.Cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Capa de servicio .
+ */
+@Service
 public class ClienteService {
+
+    @Autowired
+    private ClienteDAO clienteDAO;
+
+    /**
+     * Guarda un cliente.
+     */
+    public void guardar(Cliente cliente) {
+
+        clienteDAO.guardar(cliente);
+    }
+
+    /**
+     * Devuelve la lista de clientes.
+     */
+    public List<Cliente> listar() {
+
+        return clienteDAO.listar();
+    }       
 }
