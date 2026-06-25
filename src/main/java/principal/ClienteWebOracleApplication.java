b4636b4b65598f04745647be4a0324e1da4edbde
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-06-25 15:56:05 -0500

    commit - ClienteWebOracleApplication .

--- src/main/java/principal/ClienteWebOracleApplication.java
+++ src/main/java/principal/ClienteWebOracleApplication.java
@@ -3,11 +3,16 @@ package principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  Clase principal de la aplicación .
+ */
 @SpringBootApplication
 public class ClienteWebOracleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClienteWebOracleApplication.class, args);
+		SpringApplication.run(
+				ClienteWebOracleApplication.class,
+				args
+		);
 	}
-
 }
